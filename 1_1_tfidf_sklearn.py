13fa84f07608195280e9584f9b95388a4e598788
hanlintang <taphysky@gmail.com>
2020-12-23 15:30:31 +0800

    complete tfidf-sklearn

--- 1_1_tfidf_sklearn.py
+++ 1_1_tfidf_sklearn.py
@@ -35,4 +35,25 @@ def show_tfidf(tfidf, vocb, filename):
 
 
 # 用sklearn的sparse matrix，只记录有数值的地方，防止过度占用内存
+if __name__ == "__main__":
+    # tfidfvectorizer是tfidf transformer和countvectorizer（文档版词袋模型的向量）
+    #先转成向量再算tfidf
+    vectorizer = TfidfVectorizer()
+    tf_idf = vectorizer.fit_transform(docs)
+    # from sklearn.feature_extraction.text import TfidfTransformer
+    #transformer = TfidfTransformer()
+    #tf_idf1 = transformer.fit_transform(docs)
+    print("idf: ", [(n, idf) for idf, n in zip(vectorizer.idf_, vectorizer.get_feature_names())])
+    print("w2i: ", vectorizer.vocabulary_)
 
+    query = "I get a coffee cup"
+    # 此处不能再用fit_transform，fit只能在训练集当中用
+    qtf_idf = vectorizer.transform([query])
+    res = cosine_similarity(tf_idf, qtf_idf)
+    res = res.ravel().argsort()[-3:]
+    print("\ntop 3 docs for '{}':\n{}".format(query, [docs[i] for i in res[::-1]]))
+    #print(tf_idf)
+
+    i2w = {i:w for w, i in vectorizer.vocabulary_.items()}
+    # sklearn默认是一个sparse matrix, 要转成dense matrix
+    show_tfidf(tf_idf.todense(), [i2w[i] for i in range(len(i2w))], "tfidf_sklearn_matrix")
